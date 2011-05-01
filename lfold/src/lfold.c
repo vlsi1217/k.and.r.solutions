c5c5b246cba9359530c2856b876d779445d5e92c
Rany Keddo <rany@soundcloud.com>
2011-05-01 20:19:20 +0200

    lfold completed

---
+++ lfold/src/lfold.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DELIMITER ' '
+#define END_OF_STREAM -1
+#define LINE_WIDTH 80
+#define MAX_WORD_LENGTH 100
+#define SPACE_WIDTH 1
+
+int main() {
+  int remaining = LINE_WIDTH;
+  char word[MAX_WORD_LENGTH];
+
+  while(scan(word) > END_OF_STREAM) {
+    int width = strlen(word);
+
+    if(width > remaining) {
+      printf("\n");
+      remaining = LINE_WIDTH - width;
+
+    } else {
+      remaining = remaining - width - SPACE_WIDTH;
+    }
+
+    printf("%s", word);
+    printf(" ");
+  }
+
+  exit(EXIT_SUCCESS);
+}
+
+/* scanning state. waiting for beginning of word */
+
+int scan(char word[]) {
+  int character;
+
+  while(character = getchar()) {
+    if(character == EOF) {
+      return END_OF_STREAM;
+    }
+
+    if(character != DELIMITER) {
+      word[0] = character;
+      return buffer(word);
+    }
+  }
+}
+
+/* buffering state. waiting for end of word */
+
+int buffer(char word[]) {
+  int character, index = 1;
+
+  while(character = getchar()) {
+    if(character != DELIMITER && character != EOF && index < MAX_WORD_LENGTH) {
+      word[index] = character;
+
+    } else {
+      word[index] = '\0';
+      return strlen(word);
+    }
+
+    index++;
+  }
+}
