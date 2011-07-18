f60aa23e4a2e98a68e70d894a65d23a7ccb8803d
Rany Keddo <rany@soundcloud.com>
2011-07-18 19:02:31 +0200

    added instrumentation tools

--- Vagrant.sh
+++ Vagrant.sh
@@ -4,7 +4,7 @@
 
 which apt-get && {
   sudo apt-get -y install curl vim ack-grep
-  sudo apt-get -y install valgrind
+  sudo apt-get -y install valgrind kcachegrind
 }
 
 # roundup
@@ -12,4 +12,10 @@ which apt-get && {
 cd /tmp
 curl -L http://github.com/bmizerany/roundup/tarball/v0.0.5 | tar xvzf -
 cd bmizerany-roundup* && ./configure && make && sudo make install
+
+# papi
+
+cd /tmp
+curl -L http://icl.cs.utk.edu/projects/papi/downloads/papi-4.1.3.tar.gz | tar xvzf -
+cd papi-4.1.3 && make && sudo make install
 
