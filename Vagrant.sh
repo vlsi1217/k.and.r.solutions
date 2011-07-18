13187b9bd33ffa1c404dd26ded81d55ee6efbbb8
Rany Keddo <rany@soundcloud.com>
2011-07-18 22:26:55 +0200

    idempotent installation

--- Vagrant.sh
+++ Vagrant.sh
@@ -2,20 +2,24 @@
 
 # packages
 
-which apt-get && {
-  sudo apt-get -y install curl vim ack-grep
-  sudo apt-get -y install valgrind kcachegrind
-}
+sudo apt-get -y install curl vim ack-grep
+sudo apt-get -y install valgrind kcachegrind oprofile
 
-# roundup
+(
+  cd /tmp
 
-cd /tmp
-curl -L http://github.com/bmizerany/roundup/tarball/v0.0.5 | tar xvzf -
-cd bmizerany-roundup* && ./configure && make && sudo make install
+  # roundup
 
-# papi
+  ls bmizerany-roundup* || {
+    curl -L http://github.com/bmizerany/roundup/tarball/v0.0.5 | tar xvzf -
+    (cd bmizerany-roundup* && ./configure && make && sudo make install)
+  }
 
-cd /tmp
-curl -L http://icl.cs.utk.edu/projects/papi/downloads/papi-4.1.3.tar.gz | tar xvzf -
-cd papi-4.1.3 && make && sudo make install
+  # papi
+
+  ls papi-4.1.3 || {
+    curl -L http://icl.cs.utk.edu/projects/papi/downloads/papi-4.1.3.tar.gz | tar xvzf -
+    (cd papi-4.1.3/src && sudo ./configure && sudo make && sudo make install)
+  }
+)
 
